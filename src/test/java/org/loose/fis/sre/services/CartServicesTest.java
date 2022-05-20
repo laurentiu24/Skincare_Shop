6026c23c4b8d157221116bd67f5afd1ac34d58a9
laurentiu24 <popalaurentiu24@yahoo.com>
2022-05-20 20:40:01 +0300

    SS-44: Testare functionalitati

---
+++ src/test/java/org/loose/fis/sre/services/CartServicesTest.java
@@ -0,0 +1,24 @@
+package org.loose.fis.sre.services;
+
+import org.junit.Test;
+import org.loose.fis.sre.model.Produs;
+
+import java.nio.file.Files;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CartServicesTest {
+    @Test
+    public void testCopyDefaultUserNotExists() throws Exception{
+        ProduseService.initProdusDatabase();
+        assertTrue(Files.exists(FileSystemService.getPathToFile()));
+    }
+
+    public void addCart()
+    {
+        int i = CartServices.getAllProdus().size();
+        CartServices.addCos(new Produs("test","test","test"));
+        assertEquals(i+1,UserService.getAllUsers().size());
+    }
+}
---
+++ src/test/java/org/loose/fis/sre/services/UserServiceTest.java
@@ -0,0 +1,30 @@
+package org.loose.fis.sre.services;
+
+import org.junit.Test;
+import org.loose.fis.sre.exceptions.NoUpperCaseException;
+import org.loose.fis.sre.exceptions.UncompletedFieldsException;
+import org.loose.fis.sre.exceptions.UsernameAlreadyExistException;
+
+import java.nio.file.Files;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class UserServiceTest {
+    private UserService UserSevice;
+
+    @Test
+    public void testCopyDefaultUserNotExists() throws Exception{
+        UserSevice.initDatabase();
+        assertTrue(Files.exists(FileSystemService.getPathToFile()));
+    }
+
+    @Test
+    public void addOneUser() throws UsernameAlreadyExistException, NoUpperCaseException, UncompletedFieldsException {
+        int i = UserService.getAllUsers().size();
+        UserService.addUser("test","Test","test","test","test","test");
+        assertEquals(i+1,UserService.getAllUsers().size());
+        UserService.delete("test");
+
+    }
+}
